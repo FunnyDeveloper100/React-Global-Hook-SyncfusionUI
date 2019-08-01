17ddaa97d206facfbd7cdd12f631a6c746a10a5d
codemaster08240328 <codemaster08240328@gmail.com>
2019-08-01 16:01:53 -0300

    merge the file data every time

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
 export const setGridData = (store, data) => {
-  store.setState({ grid_data: data });
+    Array.prototype.push.apply(data, store.state.grid_data)
+    store.setState({ grid_data: data });
 };
